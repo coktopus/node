63b68ae577d7067555552c3d432cd4e1fc5e9e0c
coktopus <63889078+coktopus@users.noreply.github.com>
2020-08-15 03:38:15 +0530

    add

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3100;
 
 app.get('/', (req, res) => res.send('Welcome!'));
+app.get('/test', (req, res) => res.send('test response'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
