6c9199d7844c71a0531a490c93e8e3a9d57d5597
coktopus <63889078+coktopus@users.noreply.github.com>
2020-08-16 17:22:37 +0530

    Revert "Response update"

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Welcome to the updated container!'));
-app.get('/test', (req, res) => res.send('test response'));
-app.get('/example', (req, res) => res.send('example response'));
+app.get('/', (req, res) => res.send('Welcome!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
